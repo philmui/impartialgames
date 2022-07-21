7cbe5ef6152349dfdca3364c251fd8165df37145
HarryWangATX <harrynicomama@gmail.com>
2022-07-21 12:51:15 -0500

    Human play

--- nim-harry/nim_run.py
+++ nim-harry/nim_run.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from nim_env import NimEnv
 from nim_rl import QAgent
 
@@ -9,26 +11,54 @@ def update():
 
         env.reset()
 
-        state = env.get_state()
-
         while True:
             # print(state)
+            state = env.get_state()
             action = RL.get_action(state)
             # print(action)
             next_state, reward, game_over = env.step(action)
             # print(next_state)
             # print()
             RL.update_q_table(state, action, reward, next_state)
-            state = next_state.copy()
 
             if game_over:
                 break
 
 
+def play():
+    while True:
+        print('Current Pile: ' + str(env.get_state()))
+
+        pile = input('Enter Pile: ')
+        amt = input('Enter Amount: ')
+
+        env.update([int(pile), int(amt)])
+
+        if (np.sum(env.get_state()) == 0):
+            print('Game Over You Win!')
+            break
+
+        print('Current Pile: ' + str(env.get_state()))
+
+        action = RL.get_action(env.get_state())
+
+        print('Computer chooses from pile ' + str(action[0]) + ' with amount ' + str(action[1]))
+
+        env.update(action)
+
+        if np.sum(env.get_state()) == 0:
+            print('Game Over Computer Wins')
+            break
+
+
 if __name__ == '__main__':
-    env = NimEnv(n=3, stones_per_pile=8, max_remove=3)
+    env = NimEnv(n=3, stones_per_pile=9, max_remove=3)
     RL = QAgent(exp_rate=0.1, discount_rate=0.9, learning_rate=0.1, epsilon=0.1, nim_env=env)
 
     update()
 
     print(RL.get_q_table())
+
+    env.reset()
+
+    play()
