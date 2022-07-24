547c2f8b5938d094cfaa1408e72d9e29d00b8137
HarryWangATX <harrynicomama@gmail.com>
2022-07-23 22:50:39 -0500

    added a 3 agent implementation

--- nim-harry/nim_rl.py
+++ nim-harry/nim_rl.py
@@ -4,23 +4,33 @@ from nim_env import NimEnv
 from collections import defaultdict
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def default():
     return 0.0
 
+
 class QAgent:
-    def __init__(self, discount_rate, learning_rate, epsilon, nim_env):
+    def __init__(self, name, discount_rate, learning_rate, epsilon):
         self.discount_rate = discount_rate
         self.learning_rate = learning_rate
         self.epsilon = epsilon
-        self.nim_env = nim_env
+        self.nim_env = None
+        self.name = name
+        self.episode = 0
+        self.wins_x = []
+        self.wins_y = []
+        self.accuracy_x = []
+        self.accuracy_y = []
+        self.wins = []
+        self.accuracy = []
         self.q_table = defaultdict(partial(defaultdict, default))
 
     def get_action(self, state, play=False):
         actions = self.nim_env.get_possible_actions(state)
         if np.random.rand() < self.epsilon and not play:
-            return actions[np.random.randint(0, len(actions))]
+            action = actions[np.random.randint(0, len(actions))]
         else:
             max_val = -np.inf
             max_action = []
@@ -32,19 +42,60 @@ class QAgent:
                 elif self.q_table[str(state)][str(action)] == max_val:
                     max_action.append(action)
 
-            return max_action[np.random.randint(0, len(max_action))]
+            action = max_action[np.random.randint(0, len(max_action))]
 
-    def update_q_table(self, state, action, reward, next_state):
+        opt_act = self.nim_env.get_optimal_action(state)
+
+        self.accuracy.append(1 if action in opt_act else 0)
+
+        return action
+
+    def update_q_table(self, state, action, reward, next_state, flagged=False):
         q_predict = self.q_table[str(state)][str(action)]
+
+        multipler = 0.2 if flagged else 1
+
         if np.sum(next_state) == 0:
+            self.wins.append(1 if reward > 0 else 0)
             q_target = reward
         else:
             q_target = reward + self.discount_rate * (0 if len(self.q_table[str(next_state)]) == 0 else max(self.q_table[str(next_state)].values()))
 
-        self.q_table[str(state)][str(action)] = (1 - self.learning_rate) * q_predict + self.learning_rate * q_target
+        self.q_table[str(state)][str(action)] = (1 - multipler * self.learning_rate) * q_predict + multipler * self.learning_rate * q_target
 
     def get_q_table(self):
         return self.q_table
 
+    def set_env(self, env):
+        self.nim_env = env
+
+    def get_name(self):
+        return self.name
+
     def set_epsilon(self, epsilon):
         self.epsilon = epsilon
+
+    def add_points(self):
+        self.episode += 1
+        self.update_accurate()
+        self.update_win()
+
+    def update_win(self):
+        if self.episode % 500 == 0:
+            self.wins_x.append(self.episode)
+            self.wins_y.append(np.mean(self.wins))
+            self.wins = []
+
+    def update_accurate(self):
+        if self.episode % 500 == 0:
+            self.accuracy_x.append(self.episode)
+            self.accuracy_y.append(np.mean(self.accuracy))
+            self.accuracy = []
+
+    def plot(self, title):
+        # print(self.wins)
+        plt.plot(self.accuracy_x, self.accuracy_y, label='Accuracy Rate', color='red')
+        plt.plot(self.wins_x, self.wins_y, label='Win Rate', color='blue')
+        plt.legend()
+        plt.title(title)
+        plt.show()
